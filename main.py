c7f99c8b870526fffe01c2d86d26779d81d4df9e
sin1ter <its.symonbarua@gmail.com>
2025-05-18 22:50:39 +0600

    Updated CI

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from summarizer.keyword_analizer import extract_keywords
 
 if __name__ == "__main__":
     user_msgs, ai_msgs = read_chat_log("Assignment/chat.txt")
-    # print(user_msgs)
-    # print(ai_msgs)
-    # print(calculate_statistics(user_msgs, ai_msgs))
+    print(user_msgs)
+    print(ai_msgs)
+    print(calculate_statistics(user_msgs, ai_msgs))
     print(extract_keywords(user_msgs, ai_msgs))
